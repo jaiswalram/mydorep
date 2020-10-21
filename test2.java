cb00a1d5c9b974ff30a4a8364305618c10adcaa9
Ram Jaiswal <ram.c.jswl@gmail.com>
2020-10-21 00:26:28 -0700

    sfsalkfjsalkjdflsakjfsafk

--- test2.java
+++ test2.java
@@ -1,7 +1,15 @@
 public class test2
 {
 public static void main(arg [])
+
 {
-System.out.println("Newtestclassfor show");
+
+
+
+System.out.println("Modified the class");
+
+
+
+System.out.println("New test class for show");
 }
 }
---
+++ test4.java
@@ -0,0 +1,8 @@
+public class test4
+{
+
+public ststic void main(String args[])
+{
+System.out.println("Test4 java class");
+}
+}
